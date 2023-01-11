e43f0d540ac8ed2deed591177ab17b7f90f0593c
mina <mina899432@gmail.com>
2023-01-11 17:49:46 +0200

    fix

--- app/Http/Controllers/Admin/IconCrudController.php
+++ app/Http/Controllers/Admin/IconCrudController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Requests\admin\IconRequest;
+use App\Http\Requests\Admin\IconRequest;
 use App\Models\Icon;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
